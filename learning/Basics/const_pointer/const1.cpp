ccbd4a3b07af315c94f146d9b1905bef9b5ca71d
Suraj Tupkar <surajtupkar07@gmail.com>
2026-08-11 23:56:06 +0530

    const

--- learning/Basics/const_pointer/const1.cpp
+++ learning/Basics/const_pointer/const1.cpp
@@ -11,6 +11,15 @@ int main()
     int a = 10;
     int b = 20;
     const int *p = &a;
+    // *p = 30;
+    p =&b;
+
+    // const to pointer
+
+    int c = 10;
+    int *const q = &c;
+    *q = 50;
+   //  q = &b;   // Not Allowed because const to pointer which is not allowed to change the address
 
 
 
