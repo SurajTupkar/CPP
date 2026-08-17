ef535d07e87478237942fbbfc17efc702e3d7ec0
Suraj Tupkar <surajtupkar07@gmail.com>
2026-08-17 23:50:24 +0530

    STL

--- STL/Containers/vector/vect1.cpp
+++ STL/Containers/vector/vect1.cpp
@@ -12,9 +12,16 @@ Containers :
     -> provide standardized way to store,retrieve and manipulate data in various ways.
     -> vector,list,queue,stack,set,map etc.
 
+
+1) vector :
+    -> Dynamic array that can grow or shrink in size.
+    -> Allow fast random access to elements
+    -> Insertion and removal of elements at the end is succifient    
+
 */
 int main()
 {
+
 
 
 }
