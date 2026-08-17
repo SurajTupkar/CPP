4d7e6558f42f4c82a27746c265cd4b3635f7ba89
Suraj Tupkar <surajtupkar07@gmail.com>
2026-08-17 23:47:23 +0530

    STL

---
+++ STL/Containers/vector/vect1.cpp
@@ -0,0 +1,19 @@
+#include "iostream"
+using namespace std;
+
+
+/*
+
+STL 
+    -> Standard Template Library
+
+Containers :
+    -> are classes or data structures that are designes to store and manage collection of objects.
+    -> provide standardized way to store,retrieve and manipulate data in various ways.
+
+*/
+int main()
+{
+
+
+}
