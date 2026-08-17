96f14b303e91cebc20cd0abc8b91f8ff92e76653
Suraj Tupkar <surajtupkar07@gmail.com>
2026-08-17 23:48:36 +0530

    STL

--- STL/Containers/vector/vect1.cpp
+++ STL/Containers/vector/vect1.cpp
@@ -10,6 +10,7 @@ STL
 Containers :
     -> are classes or data structures that are designes to store and manage collection of objects.
     -> provide standardized way to store,retrieve and manipulate data in various ways.
+    -> vector,list,queue,stack,set,map etc.
 
 */
 int main()
