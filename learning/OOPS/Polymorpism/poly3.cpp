bf4775a18b1881a0095b7044dccb3e2ec9b28b9d
Suraj Tupkar <surajtupkar07@gmail.com>
2026-08-19 00:52:08 +0530

    poly

--- learning/OOPS/Polymorpism/poly3.cpp
+++ learning/OOPS/Polymorpism/poly3.cpp
@@ -1,5 +1,54 @@
 /*
 Function overriding :
-
+    ->
 
 */
+
+#include "iostream"
+using namespace std;
+
+
+class Vehicle
+{
+    public:
+  virtual void start()
+    {
+        cout<<"vehicle start"<<endl;
+    }
+};
+
+class Car:public Vehicle
+{
+    public:
+    void start()
+    {
+        cout<<"car start"<<endl;
+    }
+
+};
+
+class Bus:public Vehicle
+{
+    public:
+    void start()
+    {
+        cout<<"Bus start"<<endl;
+    }
+};
+
+int main()
+{
+
+    Vehicle *p = new Car();
+    Vehicle *q = new Bus();
+    p->start();
+    q->start();
+
+    // Car obj1;
+    // obj1.start();
+    // Bus obj2;
+    // obj2.start();
+
+
+    return 0;
+}
