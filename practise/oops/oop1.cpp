c476e41bbfd29fcb270566d201ebd7d4927b04e8
Suraj Tupkar <surajtupkar07@gmail.com>
2026-08-18 21:20:20 +0530

    oops practise

--- practise/oops/oop1.cpp
+++ practise/oops/oop1.cpp
@@ -1,3 +1,12 @@
+/*
+Ambiguity in multiple inheritance
+    -> What is Multiple Inheritance
+        -> I have 2 base class and a derived class
+        -> So if i have similar method in both base class and i created object of derived class and caling that method 
+        our compiler gets confuse to call which method so scope resolution operator resolve this ambiguity.
+        -> Can see in below's example
+
+*/
 #include "iostream"
 using namespace std;
 
