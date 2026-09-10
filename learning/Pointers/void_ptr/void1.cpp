afaa4795f02242a4134f60efee51e68509552272
Suraj Tupkar <surajtupkar07@gmail.com>
2026-09-10 23:50:31 +0530

    Void/generic ptr

--- learning/Pointers/void_ptr/void1.cpp
+++ learning/Pointers/void_ptr/void1.cpp
@@ -13,6 +13,17 @@ int main()
     cout<<*(static_cast<int*>(ptr))<<endl;
 
 
+    char ch = 'A';
+    void* ptr1 = &ch;
+    cout<<"value of ch:"<<ch<<endl;
+    cout<<"value of ch:"<<*(static_cast<char*>(ptr1))<<endl;
+
+    double b = 12.90;
+    void* ptr3 = &b;
+    cout<<"value of b:"<<b<<endl;
+    cout<<"value of b:"<<*(static_cast<double*>(ptr3))<<endl;
+
+
 
 
 
