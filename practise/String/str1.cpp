373535a35ac196bf296702c911b181ce16d808d3
Suraj Tupkar <98230459+SurajTupkar@users.noreply.github.com>
2026-09-13 17:51:04 +0000

    string_questions

--- practise/String/str1.cpp
+++ practise/String/str1.cpp
@@ -20,12 +20,41 @@ void reverse_1(string str,int size)
     }
 }
 
+// check palindrome
+bool check_palindrome(string str,int size)
+{
+    int start = 0;
+    int end = size-1;
+    while(start<end)
+    {
+        if(str[start]!=str[end])
+        {
+            return false;
+        }
+        start++;
+        end--;
+    }
+    return true;
+
+}
+
 
 int main()
 {
     string str = "Palindrome";
 
     reverse_1(str,size(str));
+    cout<<endl;
+
+    string str1 = "nayan";
+    if(check_palindrome(str1,size(str1)))
+    {
+        cout<<"Palindrome string"<<endl;
+    }
+    else
+    {
+        cout<<"Not Palindrome"<<endl;
+    }
     
    
 
