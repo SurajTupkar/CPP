edfbc7ef1111401ea45f4aced12382cdbc6c1496
Suraj Tupkar <surajtupkar07@gmail.com>
2026-09-25 21:08:51 +0530

    Array_Pract_Questions

--- practise/Arrays/pract_arr7.cpp
+++ practise/Arrays/pract_arr7.cpp
@@ -89,6 +89,94 @@ class questions
         cout<<"avg:"<<sum/size<<endl;
    }
 
+   /*
+        7. Search for a given element (Linear Search).
+        8. Find the index of a given element.
+        11. Count the occurrences of a given element.
+   */
+
+   void search(int arr[],int size,int key)
+   {
+        int occ = 0;
+        for(int i=0;i<size;i++)
+        {
+            if(arr[i]==key)
+            {
+                occ++;
+                cout<<"present at index: "<<i<<endl;
+                break;
+            }
+        }
+        cout<<"occurence of given element:"<<occ<<endl;
+   }
+
+/*
+
+    9. Reverse an array.
+	    -> using Swap
+		    -> with while loop, Time Complexity : O(n)
+		    -> with for loop,   Time complexity : O(n)
+
+*/
+
+   void reverse_1(int arr[],int size)
+   {
+        int start = 0;
+        int end = size-1;
+        while(start<end)
+        {
+            swap(arr[start],arr[end]);
+            start++;
+            end--;
+        }
+
+        for(int i=0;i<size;i++)
+        {
+            cout<<arr[i]<<" ";
+        }
+   }
+
+   void reverse_2(int arr[],int size)
+   {
+    for(int i=0,j=size-1;i<j;i++,j--)
+    {
+        swap(arr[i],arr[j]);
+    }
+
+    for(int i=0;i<size;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+
+   }
+
+   /*
+    12. Find the second largest element.
+    13. Find the second smallest element.
+   */
+
+   void sec_max_min(int arr[],int size)
+   {
+        int first_min = INT_MAX;
+        int sec_min = INT_MAX;
+        for(int i=0;i<size-1;i++)
+        {
+            if(arr[i]<first_min)
+            {
+                sec_min = first_min;
+                first_min = arr[i];
+            }
+            else if(arr[i]<sec_min)
+            {
+                sec_min = arr[i];
+            }
+        }
+
+        cout<<"first_min:"<<first_min<<endl;
+        cout<<"sec_min:"<<sec_min<<endl;
+
+   }
+
 };
 
 
@@ -97,6 +185,23 @@ int main()
     questions* ptr = new questions();
     int arr[] = {1,2,3,4,-4,-2,9};
     ptr->max_min(arr,size(arr));
+
+    int key;
+    cout<<"Enter a key to search in array:";
+    cin>>key;
+    ptr->search(arr,size(arr),key);
+
+    int arr1[] = {1,2,3,4};
+    ptr->reverse_1(arr1,size(arr1));
+    cout<<endl;
+    int arr2[] = {1,2,3,4,5};
+    ptr->reverse_2(arr2,size(arr2));
+
+    cout<<endl;
+    int arr3[] = {1,2,3,4,5};
+    ptr->sec_max_min(arr3,size(arr3));
+    
+
 
 
 
